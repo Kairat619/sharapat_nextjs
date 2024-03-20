09b3e605fc88dab2beefd6f0dfec94dd7989ae01
Kairat619 <wkz777@gmail.com>
2024-03-20 14:13:11 +0600

    try,try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production'
  
-module.exports = {
+module.exports = { 
+
+
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://farzaa.vercel.app' : undefined,
 }
+
+//   module.exports = {eslint:{
+ // ignoreDuringBuilds:true,
+//   },
+//  }
