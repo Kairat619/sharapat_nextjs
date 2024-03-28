0a45c75f6aa495d68ce5b542e79d813679f38633
Kairat619 <wkz777@gmail.com>
2024-03-28 16:24:35 +0600

    config1 updated

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://sharapat1.vercel.app/' : undefined,
+  assetPrefix: isProd ? 'https://sharapat.vercel.app/' : undefined,
 }
